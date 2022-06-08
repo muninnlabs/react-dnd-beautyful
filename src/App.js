3e8c77932198b1e653d45dc710866791599dd32e
DESKTOP-C0E4755\Alexandre Rodrigues <alexandre.rodrigues@alter-solutions.com>
2022-06-08 17:28:58 +0200

    Implement DnD Beautyful

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import './css/App.css';
+import { List } from 'antd';
+import 'antd/dist/antd.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { useState } from 'react';
+
+const someData = [
+    {
+        id: 'gary',
+        sort: 1,
+        name: 'Gary Goodspeed',
+        thumb: '/images/gary.png'
+    },
+    {
+        id: 'cato',
+        sort: 0,
+        name: 'Little Cato',
+        thumb: '/images/cato.png'
+    },
+
+    {
+        id: 'kvn',
+        sort: 2,
+        name: 'KVN',
+        thumb: '/images/kvn.png'
+    },
+    {
+        id: 'mooncake',
+        sort: 3,
+        name: 'Mooncake',
+        thumb: '/images/mooncake.png'
+    },
+    {
+        id: 'quinn',
+        sort: 4,
+        name: 'Quinn Ergon',
+        thumb: '/images/quinn.png'
+    }
+];
+
+export default function App() {
+    const [listItems, setListItems] = useState(someData);
+
+    function handleOnDragEnd(result) {
+        if (!result.destination) return;
+
+        const clonedListItems = JSON.parse(JSON.stringify(listItems));
+        const [reorderedItem] = clonedListItems.splice(result.source.index, 1);
+        clonedListItems.splice(result.destination.index, 0, reorderedItem);
+
+        setListItems(clonedListItems);
+        console.log('handleOnDragEnd', clonedListItems);
+    }
+
+    return (
+        <div className='App'>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId='DropId'>
+                    {(provided) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps}>
+                            <List
+                                bordered
+                                rowKey='Id'
+                                dataSource={listItems}
+                                size='small'
+                                renderItem={(item, index) => (
+                                    <Draggable draggableId={`draggable-${index}`} index={index}>
+                                        {(provided) => (
+                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                <List.Item>{item.name}</List.Item>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                )}
+                            />
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </div>
+    );
+}
